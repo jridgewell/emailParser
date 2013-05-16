51003cdf8b9f87903e6ede5259e72eae0edd28fe
Justin Ridgewell <justin@ridgewell.name>
2013-05-16 03:56:53 -0400

    Cleanup gem

--- ruby/lib/email_parser.rb
+++ ruby/lib/email_parser.rb
@@ -9,8 +9,10 @@ class EmailParser
     @raw = raw.lines.to_a
     @headers = {}
     @attachments = []
+    
     parse_headers
     parse_body
+    
     @raw = nil
   end
 
@@ -20,59 +22,91 @@ class EmailParser
   end
 
   private
+
+  ####################
+  # Header Functions #
+  ####################
   def parse_headers
     last_header = nil
     @raw.each_with_index do |unstripped_line, index|
       line = unstripped_line.strip
-      if is_blank_line? line
+      if end_of_headers? line
         @raw.shift index + 1
         break
-      elsif is_new_header? unstripped_line
-        header, value = extract_header_value(line)
-        header = header.downcase.to_sym
-        last_header = header
-        @headers[header] ||= []
-        @headers[header] << clean(value)
+      elsif new_header? unstripped_line
+        last_header = parse_header(line)
       else
-        @headers[last_header] << clean(line)
+        append_to_header last_header, line
       end
     end
   end
+  
+  def parse_header(line)
+    header, value = parse_header_and_value(line)
+    if header && value
+      header = header.downcase.to_sym
+      @headers[header] ||= []
+      append_to_header header, value
+      header
+    end
+  end
+  
 
+  ##################
+  # Body Functions #
+  ##################
   def parse_body
     content_type = header 'content-type'
     if content_type && content_type.match(/boundary=(.+)/)
       boundary = $1.gsub(/['"]/, '')
-      @raw.shift(next_boundary_index(@raw, boundary) + 1)
-      while (index = next_boundary_index(@raw, boundary))
-        raw = @raw.shift(index)
-        @raw.shift
-        @attachments << EmailParser.new(raw.join)
-      end
-      @attachments.shift.tap do |main|
-        @body = main.body
-        @attachments = main.attachments.concat @attachments
-        @headers = main.headers.merge @headers
-      end
+      parse_multipart_body boundary
     else
-      raw = @raw.join
-      body = case header('content-transfer-encoding')
-              when 'quoted-printable'
-                raw.unpack 'M'
-              when 'base64'
-                raw.unpack 'm'
-              else
-                [raw]
-              end
-      @body = body.first.strip
+      parse_single_body
     end
   end
+  
+  def parse_single_body
+    raw = @raw.join
+    body = case header('content-transfer-encoding')
+           when 'quoted-printable'
+             raw.unpack 'M'
+           when 'base64'
+             raw.unpack 'm'
+           else
+             [raw]
+           end
+    @body = body.first.strip
+  end
+  
+  def parse_multipart_body(boundary)
+    @raw.shift(next_boundary_index(@raw, boundary) + 1)
+    
+    while (index = next_boundary_index(@raw, boundary))
+      raw = @raw.shift(index)
+      @raw.shift
+      @attachments << EmailParser.new(raw.join)
+    end
+    
+    @attachments.shift.tap do |main|
+      @body = main.body
+      @attachments = main.attachments.concat @attachments
+      @headers = main.headers.merge @headers
+    end
+  end
+  
 
-  def is_blank_line?(line)
+  #####################
+  # Utility Functions #
+  #####################
+  def blank_line?(line)
     line == ''
   end
 
-  def is_new_header?(line)
+  def end_of_headers?(line)
+    blank_line? line
+  end
+
+  def new_header?(line)
     line =~ /^[^\s]/
   end
 
@@ -80,7 +114,7 @@ class EmailParser
     array.index { |value| value =~ /-*#{boundary}-*/ }
   end
 
-  def extract_header_value(line)
+  def parse_header_and_value(line)
     matches = line.match(/^([^:]+):\s*(.*)$/)
     if matches
       matches = matches.to_a
@@ -89,7 +123,11 @@ class EmailParser
     end
   end
 
+  def append_to_header(header, value)
+    @headers[header] << clean(value)
+  end
+
   def clean(line)
-    line.gsub(/,\s*/, '')
+    line.gsub(/,\s*$/, '')
   end
 end
